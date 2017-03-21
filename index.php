42f7e435b76d41df7e87a7dc4537b706d0dae998
alumno <alumno>
2017-03-20 21:30:27 -0300

    Forma numero 1. Array con constructor

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
-echo "Hola mundo"
+echo "Hola mundo". "<br>";
+
+//Ejercicio N° 6 
+//Forma 1: Creando un array con contructor
+
+$array = array(
+
+    rand(0,150),
+    rand(0,150),
+    rand(0,150),
+    rand(0,150),
+    rand(0,150)
+);
+
+var_dump($array);
+
+echo "<br><br>"
 
 ?>
